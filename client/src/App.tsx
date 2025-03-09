448663565bbf4d4d9d12b5a772b0add6723c9d74
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-03-09 14:46:06 +0530

    Cln and Add NavBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,66 @@
-function App() {
-  return <h1>HI</h1>;
-}
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@heroui/react";
 
-export default App;
+export default function App() {
+  return (
+    <Navbar className="bg-background/70 backdrop-blur-lg" maxWidth="xl">
+      <NavbarBrand>
+        <p className="font-bold text-xl text-inherit">StudyBuddy</p>
+      </NavbarBrand>
+
+      <NavbarContent className="gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Dashboard
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Study Groups
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Resources
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Community
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button
+            as={Link}
+            className="font-medium"
+            color="primary"
+            href="#"
+            variant="flat"
+          >
+            Login
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button
+            as={Link}
+            className="font-medium"
+            color="primary"
+            href="#"
+            variant="solid"
+          >
+            Let &apos; s Get started
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
