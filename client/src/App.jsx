90aa3d575567e0c0eb6b1a694eed159f3bbeef6c
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-05-04 18:43:47 +0530

    working on dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import LandingPage from "./pages/Landing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Student from './pages/student'
 import DashboardLayout from './layouts/Dashboard'
+import Notes from "./pages/student/notes";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/student" element={<DashboardLayout/>}>
           <Route index element={<Student />} />
+          <Route path="/student/notes" element={<Notes />} />
         </Route>
         <Route path="/" element={<LandingPage />} />
       </Routes>
