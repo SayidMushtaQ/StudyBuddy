58ae4662ecd2bd687c116e40afe1323bf66cc12b
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-05-05 17:43:28 +0530

    start with teacher dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,25 @@
 import LandingPage from "./pages/Landing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/student/StudentHome'
+import StudentHome from './pages/student'
 import DashboardLayout from './layouts/Dashboard'
 import Notes from "./pages/student/Notes/Index";
 import CreateOwnNotes from "./pages/student/Notes/OwnNotes";
 import ClassNotes from "./pages/student/Notes/ClassNotes";
+
+import TeacherHome from './pages/teacher'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/student" element={<DashboardLayout/>}>
-          <Route index element={<Home />} />
+          <Route index element={<StudentHome />} />
           <Route path="/student/notes" element={<Notes />} />
           <Route path="/student/notes/create" element={<CreateOwnNotes />} />
           <Route path="/student/notes/class" element={<ClassNotes/>} />
+        </Route>
+        <Route path="/teacher" element={<DashboardLayout/>}>
+          <Route index element={<TeacherHome />} />
         </Route>
         <Route path="/" element={<LandingPage />} />
       </Routes>
