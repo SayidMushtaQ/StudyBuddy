de7d52cb74d7c96eca8160b09bf767a00ddfb58c
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-05-07 19:06:23 +0530

    add

--- server/src/controllers/auth/verifyOTP.controller.js
+++ server/src/controllers/auth/verifyOTP.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../../util/asyncHandler.util.js";
 import { apiResponse } from "../../util/apiResponse.utils.js";
 import { ApiError } from "../../util/apiError.utils.js";
 import { User } from "../../modules/userAuth.model.js";
-
+import jwt from "jsonwebtoken";
 export const verifyOTP = asyncHandler(async (req, res) => {
   const { userId, otp } = req.body;
 
@@ -20,6 +20,12 @@ export const verifyOTP = asyncHandler(async (req, res) => {
   user.otp = undefined;
   await user.save();
 
+  const token = jwt.sign(
+    { userId, phone: user.phone },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
   res.status(200).json(
     new apiResponse(
       200,
@@ -28,7 +34,8 @@ export const verifyOTP = asyncHandler(async (req, res) => {
           id: user._id,
           name: user.name,
           phone: user.phone,
-          role: user.role
+          role: user.role,
+          jwt_token: token
         }
       },
       "OTP verified successfully. Registration complete."
