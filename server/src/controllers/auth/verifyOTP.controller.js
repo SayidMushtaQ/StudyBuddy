c3745f4a24fe95121e4f2866d42231fd2de39449
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-05-09 10:12:28 +0530

    add middleware

--- server/src/controllers/auth/verifyOTP.controller.js
+++ server/src/controllers/auth/verifyOTP.controller.js
@@ -25,6 +25,12 @@ export const verifyOTP = asyncHandler(async (req, res) => {
     process.env.JWT_SECRET,
     { expiresIn: "7d" }
   );
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    sameSite: "strict"
+  });
 
   res.status(200).json(
     new apiResponse(
---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+import { asyncHandler } from "../util/asyncHandler.util.js";
+import { apiResponse } from "../util/apiResponse.utils.js";
+import { ApiError } from "../util/apiError.utils.js";
+import { User } from "../modules/user.model.js";
+
+export const authenticateUser = asyncHandler(async (req, res) => {
+  let token;
+
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
+  if (!token) {
+    throw new ApiError(401, "You are not authorized to access this resource");
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  console.log(decoded);
+  const user = await User.findById(decoded.userId);
+
+  if (!user) {
+    throw new ApiError(401, "You are not authorized to access this resource");
+  }
+  req.user = {
+    id: user._id,
+    phoneNumber: user.phoneNumber,
+    verified: user.verified,
+    role: user.role
+  };
+  next();
+});
